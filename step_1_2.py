5b7dd0d24997ed744e6db8cf2a692f55de9d8fc4
shroh12 <ns50811@naver.com>
2025-03-27 16:06:26 +0900

    Update step_1_2.py

--- step_1_2.py
+++ step_1_2.py
@@ -1,8 +1,16 @@
+import base64
 from pathlib import Path
 import google.generativeai as genai
 import streamlit as st 
 from PIL import Image, ImageFile
 from step_1_1 import OUT_DIR
+
+def img_to_base64(img: Image.Image) -> str:
+    import io
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    byte_data = buf.getvalue()
+    return base64.b64encode(byte_data).decode()
 
 def get_model(sys_prompt: str = None) -> genai.GenerativeModel:
     GEMINI_KEY = "AIzaSyAOFZ1Gy2E9jrGI4p92jNEtjOLZ9MDojh0"
@@ -10,7 +18,7 @@ def get_model(sys_prompt: str = None) -> genai.GenerativeModel:
     genai.configure(api_key=GEMINI_KEY, transport="rest")
     return genai.GenerativeModel(GEMINI_MODEL,
                                  system_instruction=sys_prompt)
-    
+
 def uploaded_image(on_change=None, args=None) -> Image.Image | None:
     with st.sidebar:
         # 중앙 정렬 + 굵은 텍스트
@@ -18,16 +26,18 @@ def uploaded_image(on_change=None, args=None) -> Image.Image | None:
             "<div style='text-align: center; font-weight: bold; font-size: 18px;'>이미지 붙여넣기</div>",
             unsafe_allow_html=True
         )
+
+        # angmose.jpg 이미지 삽입
         img = Image.open('img/angmose.jpg').resize((500, 500))
         st.markdown(
             f"""
-            <div style="text-align: center;">
-            <img src="data:image/png;base64,{img_to_base64(img)}" width="500"/>
+            <div style="text-align: center; padding-bottom: 10px;">
+                <img src="data:image/png;base64,{img_to_base64(img)}" width="300"/>
             </div>
             """,
             unsafe_allow_html=True
-         )
-            
+        )
+
         uploaded = st.file_uploader(
             label="",  # 빈 라벨
             label_visibility="collapsed",
@@ -42,9 +52,9 @@ def uploaded_image(on_change=None, args=None) -> Image.Image | None:
                 img = Image.open(tmp_path)
                 st.image(img, use_column_width=True)
                 return img
-            
+
 if __name__ == "__main__":
-    st.set_page_config(layout="wide")
+    st.set_page_config(page_title="영어 받아쓰기 연습", layout="wide", page_icon="🦜")
     st.title("✨ 만들면서 배우는 멀티모달 AI")
     if img := uploaded_image():
         prompt = "공연은 어디에서 몇 시에 시작해? 한글로 대답해 줘."
@@ -53,6 +63,9 @@ if __name__ == "__main__":
         with st.chat_message("✨"):
             with st.spinner("대화를 생성하는 중입니다."):
                 model = get_model()
+                chat = model.start_chat()
+                resp = chat.send_message([img, prompt])
+                st.markdown(resp.text)
                 chat = model.start_chat()
                 resp = chat.send_message([img, prompt])
                 st.markdown(resp.text)
