7421cd8fb0bdc7d09fa20f413fa1408933088d04
shroh12 <ns50811@naver.com>
2025-04-08 11:00:29 +0900

    Update step_3_2.py

--- step_3_2.py
+++ step_3_2.py
@@ -28,7 +28,7 @@ def init_page():
     # 타이틀
     st.markdown(
         """
-        <h1 style='text-align: center; font-size:48px; color: #4B89DC;'>🔊앵무새 객관식 퀴즈</h1>
+        <h1 style='text-align: center; font-size:48px; color: #4B89DC;'>🔊앵무새 스쿨</h1>
         """, unsafe_allow_html=True)
 
     # 설명 텍스트
