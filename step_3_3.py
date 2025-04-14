c3ceb2944256b4473699816c3b6425229b972c70
shroh12 <ns50811@naver.com>
2025-04-14 14:24:17 +0900

    Update step_3_3.py

--- step_3_3.py
+++ step_3_3.py
@@ -2,7 +2,7 @@ import streamlit as st
 from step_1_2 import uploaded_image
 from step_1_3 import clear_session, init_session
 from step_3_1 import generate_feedback
-from step_3_2 import init_page, reset_quiz, set_quiz
+from step_3_2 import init_page, reset_quiz, show_score_summary, init_score, update_score, set_quiz
 import random
 import pandas as pd
 
