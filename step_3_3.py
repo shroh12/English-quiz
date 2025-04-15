a87397e4f77c56332e27e9b64a39b36494fa7ad5
shroh12 <ns50811@naver.com>
2025-04-15 14:39:43 +0900

    Update step_3_3.py

--- step_3_3.py
+++ step_3_3.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from step_1_2 import uploaded_image
 from step_1_3 import clear_session, init_session
-from step_3_1 import generate_feedback
-from step_3_2 import init_score, show_quiz, reset_quiz
+from step_3_1 import generate_quiz, generate_feedback
+from step_3_2 import img_to_base64, init_page, set_quiz, reset_quiz, show_quiz, init_score, update_score
 import pandas as pd
 
 def init_score():
