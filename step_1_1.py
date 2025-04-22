57c51d0072ca023bb17da731b4717d7c9e997a2c
shroh12 <ns50811@naver.com>
2025-04-22 11:23:02 +0900

    Delete step_1_1.py

--- step_1_1.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-wORK_DIR = Path(__file__).parent
-IMG_DIR, IN_DIR, OUT_DIR = wORK_DIR / "img", wORK_DIR / "input", wORK_DIR / "output"
-
-if __name__ == "__main__":
-    IMG_DIR.mkdir(exist_ok=True)
-    IN_DIR.mkdir(exist_ok=True)
-    OUT_DIR.mkdir(exist_ok=True)
